27bcbe6de7fc2ae1696351b55c8c6113e17477b0
wanghaiyang <wqzbxh@163.com>
2018-11-14 17:41:50 +0800

    图片替换修改测试

--- application/common/model/Operationlog.php
+++ application/common/model/Operationlog.php
@@ -19,10 +19,22 @@ Class Operationlog extends Model
 {
     /**
      * 添加操作行为
+     * @param int $user_id
+     * @param string $module
+     * @param string $controller
+     * @param string $method
+     * @param string $operate_type
+     * @param string $operate_info
      */
-
-    public static function addOperation($user_id)
+    public static function addOperation($user_id = 0,$module = '',$controller = '',$method = '',$operate_type = '',$operate_info = '')
     {
-       // $data['user_id'] = $user_id;
+        $data = [];
+        $data['user_id'] = $user_id;
+        $data['module'] = $module;
+        $data['controller'] = $controller;
+        $data['method'] = $method;
+        $data['operate_type'] = $operate_type;
+        $data['operate_info'] = $operate_info;
+        self::insert($data);
     }
 }
